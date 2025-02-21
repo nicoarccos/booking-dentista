c288ef45c54cc0b979adde5ec2574c266258b817
Joaquin Avaro Acosta <95146374+joakko18@users.noreply.github.com>
2025-02-21 16:01:47 +0200

    3Commit
    3commit

--- app/components/types.ts
+++ app/components/types.ts
@@ -1,9 +1,7 @@
-// types.ts
 export interface AvailableSlot {
-    id: number;
-    date: string;
-    day: string;
-    time_slot: string;
-    is_booked: boolean;
-  }
-  
+  id: number;
+  date: string;
+  day: string;
+  time_slot: string;
+  booked: boolean; // Renamed from is_booked to match Slot
+}
